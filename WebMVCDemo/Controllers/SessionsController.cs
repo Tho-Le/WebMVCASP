4cc31125282219b0d5f054ba1eca205241693c26
Tho <tho.le1164@gmail.com>
2020-04-06 02:19:10 -0500

    Created login_feature branch.

---
+++ WebMVCDemo/Controllers/SessionsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVCDemo.Controllers
+{
+    public class SessionsController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
