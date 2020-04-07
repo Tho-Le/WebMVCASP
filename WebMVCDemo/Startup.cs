7a61e28d90ad648f0259365069808adeb260d3dd
Tho <tho.le1164@gmail.com>
2020-04-07 04:51:19 -0500

    Added a Authentication Scheme for practice.

--- WebMVCDemo/Startup.cs
+++ WebMVCDemo/Startup.cs
@@ -25,6 +25,11 @@ namespace WebMVCDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication("CookieAuth").AddCookie("CookieAuth", config => {
+                config.Cookie.Name = "Grandmas.Cookie";
+                config.LoginPath = "/Sessions/Authenticate";
+            });
+            
             services.AddControllersWithViews();
 
             services.AddDbContext<CountryContext>(options =>
